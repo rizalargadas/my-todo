7e28f6fcfde92336869d3a489e7670b551429cc2
Riza May Largadas <rmlargadas@gmail.com>
2024-05-10 08:27:54 +0800

    uncomment csrf middleware and added CSRF_TRUSTED_ORIGIN

--- mytodo/settings.py
+++ mytodo/settings.py
@@ -29,7 +29,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-e854.up.railway.app/',]
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e854.up.railway.app/']
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
@@ -59,7 +61,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
